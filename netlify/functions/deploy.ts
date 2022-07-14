f2a8f7f8439886e6fa0b871bc96377e4241783e4
Viper-M <vipersharma.88@gmail.com>
2022-07-14 20:39:19 +0530

    Added

--- netlify/functions/deploy.ts
+++ netlify/functions/deploy.ts
@@ -1,7 +1,9 @@
 import { schedule } from "@netlify/functions";
+var count = 0
 
 export const handler = schedule("* * * * *", async (event: any) => {
-    console.log("deploying...")
+    console.log("deploying...", count)
+    count++
     return {
         statusCode: 200,
         body: "Hello from Netlify"
